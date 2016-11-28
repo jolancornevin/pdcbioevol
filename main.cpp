c3b15d09a66b60e145777fee6e8d761df067c44a
jolancornevin <cornevin.jolan@gmail.com>
2016-11-28 18:23:54 +0100

    [main] add openmp dependencies + check if it's working

--- main.cpp
+++ main.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
 #include "src/headers/World.h"
 #include "src/headers/Common.h"
+#include <omp.h>
+#include <ctime>
+
 
 using namespace std;
 
 int main() {
-  printf("Init binding matrix\n");
-  Common::init_binding_matrix(897685687);
 
-  printf("Create World\n");
-  World* world = new World(32, 32, 897986875);
+    int target_thread_num = 4;
+    omp_set_num_threads(target_thread_num);
+    unsigned long times[target_thread_num];
 
-  printf("Initialize environment\n");
-  world->init_environment();
+    clock_t begin = clock();
 
-  bool test = false;
-  if (test) {
-    world->test_mutate();
-  } else {
-    printf("Initialize random population\n");
-    world->random_population();
+    //Test if openMP is working
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        std::cout << "Thread number: " << omp_get_thread_num() << endl;
+    }
 
-    printf("Run evolution\n");
-    world->run_evolution();
-  }
+    printf("Init binding matrix\n");
+    Common::init_binding_matrix(897685687);
+
+    printf("Create World\n");
+    World *world = new World(32, 32, 897986875);
+
+    printf("Initialize environment\n");
+    world->init_environment();
+
+    bool test = false;
+    if (test) {
+        world->test_mutate();
+    } else {
+        printf("Initialize random population\n");
+        world->random_population();
+
+        printf("Run evolution\n");
+        //world->run_evolution();
+    }
+
+    clock_t end = clock();
+    std::cout << "Time : " << double(end - begin) / CLOCKS_PER_SEC << endl;
 }
