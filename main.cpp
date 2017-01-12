cb3c9a652d22f428e975de20e5a7394895af0234
ealhad <34lh4d@gmail.com>
2017-01-12 08:25:27 +0100

    openMP working

--- main.cpp
+++ main.cpp
@@ -13,77 +13,77 @@
 using namespace std;
 
 int main() {
-	char hostname[257];
-	int size, rank;
-	int i, pid;
-	int bcast_value = 1;
+    char hostname[257];
+    int size, rank;
+    int i, pid;
+    int bcast_value = 1;
 
-	gethostname(hostname, sizeof hostname);
-	MPI_Init(NULL, NULL);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	
-	if (!rank) {
-		bcast_value = 42;
-	}
-	
-	MPI_Bcast(&bcast_value,1 ,MPI_INT, 0, MPI_COMM_WORLD );
-	printf("%s\t- %d - %d - %d\n", hostname, rank, size, bcast_value);
-	fflush(stdout);
-	
-	//Test if openMP is working
-	#pragma omp parallel
-	{
-		int thread_id = omp_get_thread_num();
-		#pragma omp critical
-		{
-			std::cout << "RANK " << rank << " Thread number: " << omp_get_thread_num() << endl;
-		}
-	}
-	
-	set_rank(rank);
-	lolilol();
-	
-	if (is_master()) {
-		int target_thread_num = 4;
-		omp_set_num_threads(target_thread_num);
+    gethostname(hostname, sizeof hostname);
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-		unsigned long times[target_thread_num];
+    if (!rank) {
+        bcast_value = 42;
+    }
 
-		//  Start Timers
-		double wall0 = get_wall_time();
-		double cpu0  = get_cpu_time();
+    MPI_Bcast(&bcast_value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    printf("%s\t- %d - %d - %d\n", hostname, rank, size, bcast_value);
+    fflush(stdout);
+
+    //Test if openMP is working
+#pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+#pragma omp critical
+        {
+            std::cout << "RANK " << rank << " Thread number: " << omp_get_thread_num() << endl;
+        }
+    }
+
+    set_rank(rank);
+    lolilol();
+
+    if (is_master()) {
+        int target_thread_num = 4;
+        omp_set_num_threads(target_thread_num);
+
+        unsigned long times[target_thread_num];
+
+        //  Start Timers
+        double wall0 = get_wall_time();
+        double cpu0 = get_cpu_time();
 
 
-		printf("Init binding matrix\n");
-		Common::init_binding_matrix(897685687);
+        printf("Init binding matrix\n");
+        Common::init_binding_matrix(897685687);
 
-		printf("Create World\n");
-		World *world = new World(32, 32, 897986875);
+        printf("Create World\n");
+        World *world = new World(32, 32, 897986875);
 
-		printf("Initialize environment\n");
-		world->init_environment();
+        printf("Initialize environment\n");
+        world->init_environment();
 
-		bool test = false;
-		if (test) {
-			world->test_mutate();
-		} else {
-			printf("Initialize random population\n");
-			world->random_population();
+        bool test = false;
+        if (test) {
+            world->test_mutate();
+        } else {
+            printf("Initialize random population\n");
+            world->random_population();
 
-			printf("Run evolution\n");
-			world->run_evolution();
-		}
+            printf("Run evolution\n");
+            world->run_evolution();
+        }
 
-		//  Stop timers
-		double wall1 = get_wall_time();
-		double cpu1  = get_cpu_time();
+        //  Stop timers
+        double wall1 = get_wall_time();
+        double cpu1 = get_cpu_time();
 
-		cout << "Wall Time = " << wall1 - wall0 << endl;
-		cout << "Total CPU Time  = " << cpu1  - cpu0  << endl;
-	}
+        cout << "Wall Time = " << wall1 - wall0 << endl;
+        cout << "Total CPU Time  = " << cpu1 - cpu0 << endl;
+    }
 
-	MPI_Barrier(MPI_COMM_WORLD);
-	MPI_Finalize();
-	return 0;
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
+    return 0;
 }
